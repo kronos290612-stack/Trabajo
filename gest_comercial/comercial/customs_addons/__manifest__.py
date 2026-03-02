2ab9cca5dc2bde1624c10bdd4f56d556768b66dc
kronos290612 <kronos290612@gmail.com>
2026-03-01 19:02:06 -0600

    ss

---
+++ gest_comercial/comercial/customs_addons/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Commercial Management',
+    'version': '18.0',
+    'category': 'Human Resources',
+    'summary': 'Complete business management, applications, document management and deliveries.',
+    'description': """
+    Advanced Management of Advances and Expense Settlements
+    ======================================================
+
+     """,
+    'author': 'KRONOS',
+    'website': '',
+    'depends': ['account_area_expense', 'hr_expense'],
+    'data': [
+       'views/control_comecial_view.xml',
+
+            ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
