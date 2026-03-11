c47f2f59fffe6e0cd26bd86cb9466b6fb9b09534
kronos290612 <kronos290612@gmail.com>
2026-03-10 18:36:56 -0600

    sadsad

---
+++ com_management/gest_comercial/__manifest__.py
@@ -0,0 +1,18 @@
+{
+ 'name': 'Commercial M',
+    'version': '1.0',
+    'category': 'Human Resources/Expenses',
+    'summary': 'Complete commercial management',
+    'description': """
+   
+     """,
+    'author': 'KRONOS',
+    'depends': ['base'],
+    'data': [
+
+
+            ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
